0b02cbfcc21a7426d32e4830fa0f3f46c4551093
The Peeps191 <74725787+ThePeeps191@users.noreply.github.com>
2022-01-06 20:41:21 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+// Adding Libraries
+#include <IRremote.h>
+#include <Servo.h>
+
+// Declaring Pins
+struct sensor {
+  const int TRIG = 1;
+  const int ECHO = 2;
+}
+
+struct motors {
+  struct leftMotors {
+    const int lfm = 3;
+    const int lbm = 4; 
+    }
+    struct rightM
+}
